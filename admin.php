34901de3d80a8f9d6b2c8b1e92cf732423bdb148
Barış Atay <114981821+barmet1@users.noreply.github.com>
2023-12-17 22:34:24 +0300

    Banned.php is avaible

--- admin.php
+++ admin.php
@@ -14,15 +14,13 @@ if (isset($_SESSION['username'])) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($row) {
             $profilePhoto = $row['profilephoto'];
-            
-     
+
             $queryUserRole = "SELECT role FROM roles WHERE id = :id";
             $stmtUserRole = $dbh->prepare($queryUserRole);
-            $stmtUserRole->bindParam(':id', $row['id']); 
+            $stmtUserRole->bindParam(':id', $row['id']);
             $stmtUserRole->execute();
             $userRole = $stmtUserRole->fetch(PDO::FETCH_ASSOC)['role'];
 
-  
             if ($userRole !== 'admin') {
                 header("Location:user.php");
                 exit();
@@ -49,18 +47,15 @@ $stmtUsers = $dbh->prepare($queryUsers);
 $stmtUsers->execute();
 $userCount = $stmtUsers->fetch(PDO::FETCH_ASSOC)['userCount'];
 
-
 $queryAdmins = "SELECT COUNT(*) as adminCount FROM roles WHERE role = 'admin'";
 $stmtAdmins = $dbh->prepare($queryAdmins);
 $stmtAdmins->execute();
 $adminCount = $stmtAdmins->fetch(PDO::FETCH_ASSOC)['adminCount'];
 
-
 $queryModerators = "SELECT COUNT(*) as moderatorCount FROM roles WHERE role = 'moderator'";
 $stmtModerators = $dbh->prepare($queryModerators);
 $stmtModerators->execute();
 $moderatorCount = $stmtModerators->fetch(PDO::FETCH_ASSOC)['moderatorCount'];
-
 
 $queryPosts = "SELECT COUNT(*) as postCount FROM post";
 $stmtPosts = $dbh->prepare($queryPosts);
@@ -71,7 +66,6 @@ $queryUsersData = "SELECT * FROM user";
 $stmtUsersData = $dbh->prepare($queryUsersData);
 $stmtUsersData->execute();
 $usersData = $stmtUsersData->fetchAll(PDO::FETCH_ASSOC);
-
 
 $queryUserRole = "SELECT role FROM roles WHERE id = :id";
 $stmtUserRole = $dbh->prepare($queryUserRole);
@@ -436,95 +430,117 @@ $userRole = $stmtUserRole->fetch(PDO::FETCH_ASSOC)['role'];
             <img <?php echo 'src="' . $profilePhoto . '"' ?>
                 class=" border border-dark border-opacity-25 border-5 responsivedropdownpp" alt="123" style="" />
             <p class="text-light text-center">
-               Admin Name : <?php echo $username; ?>
+                Admin Name :
+                <?php echo $username; ?>
             </p>
         </a>
         <a href="profile.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
                 style="">Profile</button></a>
         <br>
-         <a href="admin.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
-                style="">Admin Page</button></a><br>
-            <a href="manageexplore.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
+        <a href="admin.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons" style="">Admin
+                Page</button></a><br>
+        <a href="manageexplore.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
                 style="">Manage Explore</button></a><br>
-                 <a href="posttable.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
-                style="">Post Table</button></a><br>
-                 <a href="login_logs.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
+        <a href="posttable.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons" style="">Post
+                Table</button></a><br>
+        <a href="login_logs.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
                 style="">Login Logs</button></a><br>
         <form method="post" action=""><button type="submit" name="logout"
-       
                 class="btn btn-outline-light mt-2 dropdown-content profilebuttons" style="">Logout</button>
         </form>
 
     </div>
     <div class="text-center" style="margin-left:10%;width:67%;">
-    <table class="table table-hover" style="">
-    <h5 class="h3 text-white">Statistics of Users</h5>
-  <tr>
-    <th>Admin Number</th>
-    <th>Moderator Number</th>
-    <th>User Number</th>
-    <th>Post Number</th>
-  </tr>
-  <tr>
-   <td><?php echo $adminCount; ?></td>
-            <td><?php echo $moderatorCount; ?></td>
-            <td><?php echo $userCount; ?></td>
-            <td><?php echo $postCount; ?></td>
-  </tr>
-</table>
-<br>
-  <a href="profile.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons"
-                style="">Manage Explore</button></a>
+        <table class="table table-hover" style="">
+            <h5 class="h3 text-white">Statistics of Users</h5>
+            <tr>
+                <th>Admin Number</th>
+                <th>Moderator Number</th>
+                <th>User Number</th>
+                <th>Post Number</th>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $adminCount; ?>
+                </td>
+                <td>
+                    <?php echo $moderatorCount; ?>
+                </td>
+                <td>
+                    <?php echo $userCount; ?>
+                </td>
+                <td>
+                    <?php echo $postCount; ?>
+                </td>
+            </tr>
+        </table>
         <br>
-    <h5 class="h3 text-white">Manage Users</h5>
- <table class="table table-hover" style="">
-  <tr>
-    <th>id</th>
-    <th>Username</th>
-    <th>Email</th>
-    <th>firstname</th>
-    <th>lastname</th>
-    <th>Delete User</th>
-    <th>Role</th>
-    <th>Ban</th>
-  </tr>
-  <tr>
-     <?php foreach ($usersData as $user) :
-    $rolesQuery = "SELECT role FROM roles WHERE id = :id";
-$stmtRoles = $dbh->prepare($rolesQuery);
-$stmtRoles->bindParam(':id', $user['id']);
-$stmtRoles->execute();
-$userRole = $stmtRoles->fetch(PDO::FETCH_ASSOC)['role'];
- ?>
-     
-        <tr>
-            <td><?php echo $user['id']; ?></td>
-            <td><a href="https://egoistsky.free.nf/egoist?username=<?php echo $user['username']; ?>"><?php echo $user['username']; ?></a></td>
-            <td><?php echo $user['email']; ?></td>
-            <td><?php echo $user['firstname']; ?></td>
-            <td><?php echo $user['lastname']; ?></td>
-            <form method="post" action="">
-             <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-            <td><button name="delete" class="btn btn-outline-danger">Delete</button></td>
-            </form>
-            <form method="post" action="">
-        <td>
-      <div class="input-group mt-1">
-       <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-  <input type="text" class="form-control" placeholder=""  value="<?php echo $userRole ?>" class="form-control" name="newRole" placeholder="New Role" aria-label="New Role" aria-describedby="button-addon1">
-  <button class="btn btn-outline-warning" name="editRole" type="submit" id="button-addon1">Edit Role</button>
-</div>
-            </td>
-            </form>
-              <form method="post" action="">
-             <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-            <td><button type="submit" name="Ban" class="btn btn-outline-danger">Ban</button>
-            <button type="submit" name="UnBan" class="btn btn-outline-primary">Un Ban</button></td>
-            </form>
-        </tr>
-    <?php endforeach; ?>
-</table>
-</div>
+        <a href="profile.php"><button class="btn btn-outline-light mt-2 dropdown-content profilebuttons" style="">Manage
+                Explore</button></a>
+        <br>
+        <h5 class="h3 text-white">Manage Users</h5>
+        <table class="table table-hover" style="">
+            <tr>
+                <th>id</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>firstname</th>
+                <th>lastname</th>
+                <th>Delete User</th>
+                <th>Role</th>
+                <th>Ban</th>
+            </tr>
+            <tr>
+                <?php foreach ($usersData as $user):
+                    $rolesQuery = "SELECT role FROM roles WHERE id = :id";
+                    $stmtRoles = $dbh->prepare($rolesQuery);
+                    $stmtRoles->bindParam(':id', $user['id']);
+                    $stmtRoles->execute();
+                    $userRole = $stmtRoles->fetch(PDO::FETCH_ASSOC)['role'];
+                    ?>
+
+                <tr>
+                    <td>
+                        <?php echo $user['id']; ?>
+                    </td>
+                    <td><a href="https://egoistsky.free.nf/egoist?username=<?php echo $user['username']; ?>">
+                            <?php echo $user['username']; ?>
+                        </a></td>
+                    <td>
+                        <?php echo $user['email']; ?>
+                    </td>
+                    <td>
+                        <?php echo $user['firstname']; ?>
+                    </td>
+                    <td>
+                        <?php echo $user['lastname']; ?>
+                    </td>
+                    <form method="post" action="">
+                        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                        <td><button name="delete" class="btn btn-outline-danger">Delete</button></td>
+                    </form>
+                    <form method="post" action="">
+                        <td>
+                            <div class="input-group mt-1">
+                                <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                                <input type="text" class="form-control" placeholder="" value="<?php echo $userRole ?>"
+                                    class="form-control" name="newRole" placeholder="New Role" aria-label="New Role"
+                                    aria-describedby="button-addon1">
+                                <button class="btn btn-outline-warning" name="editRole" type="submit"
+                                    id="button-addon1">Edit Role</button>
+                            </div>
+                        </td>
+                    </form>
+                    <form method="post" action="">
+                        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                        <td><button type="submit" name="Ban" class="btn btn-outline-danger">Ban</button>
+                            <button type="submit" name="UnBan" class="btn btn-outline-primary">Un Ban</button>
+                        </td>
+                    </form>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
@@ -536,18 +552,17 @@ $userRole = $stmtRoles->fetch(PDO::FETCH_ASSOC)['role'];
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 </script>
+
 </html>
 <?php
 if (isset($_POST['delete'])) {
     $userId = $_POST['id'];
 
-    
     $deleteUserRoleQuery = "DELETE FROM roles WHERE id = :id";
     $stmtDeleteUserRole = $dbh->prepare($deleteUserRoleQuery);
     $stmtDeleteUserRole->bindParam(':id', $userId);
     $stmtDeleteUserRole->execute();
 
-   
     $deleteUserQuery = "DELETE FROM user WHERE id = :id";
     $stmtDeleteUser = $dbh->prepare($deleteUserQuery);
     $stmtDeleteUser->bindParam(':id', $userId);
@@ -560,7 +575,6 @@ if (isset($_POST['editRole'])) {
     $userId = $_POST['id'];
     $newRole = $_POST['newRole'];
 
- 
     $updateRoleQuery = "UPDATE roles SET role = :newRole WHERE id = :id";
     $stmtUpdateRole = $dbh->prepare($updateRoleQuery);
     $stmtUpdateRole->bindParam(':newRole', $newRole);
